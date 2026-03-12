31d9ec9afcd82cea079abc73e7ed2579b6719fd9
Benjamin Ryder wismath <benjaminryderwismath@MacBook-Air-de-Benjamin.local>
2026-03-12 18:08:59 -0300

    agrega ruta root

--- src/server.js
+++ src/server.js
@@ -4,20 +4,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 const authRoutes = require("./routes/auth");
 const usuariosRoutes = require("./routes/usuarios");
-
-
-app.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto " + PORT);
-});
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/usuarios", usuariosRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API funcionando 🚀");
+});
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en puerto 3000");
