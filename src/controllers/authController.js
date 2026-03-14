eda9a34cabb4f7daf00deb4b6e65f3ce04afa26b
Benjamin Ryder wismath <benjaminryderwismath@MacBook-Air-de-Benjamin.local>
2026-03-14 13:30:17 -0300

    fix register controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,26 @@
-
 const pool = require("../config/db");
 const bcrypt = require("bcrypt");
 
 const registrar = async (req, res) => {
+    try {
     const { nombre, email, password } = req.body;
+
+    if (!nombre || !email || !password) {
+        return res.status(400).json({ error: "Faltan datos" });
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
-    "INSERT INTO usuarios (nombre,email,password) VALUES ($1,$2,$3) RETURNING id,nombre,email",
-    [nombre, email, hashedPassword]
+        "INSERT INTO usuarios (nombre,email,password) VALUES ($1,$2,$3) RETURNING id,nombre,email",
+        [nombre, email, hashedPassword]
     );
 
-    res.json(result.rows[0]);
+    return res.status(201).json(result.rows[0]);
+    } catch (error) {
+    console.error("ERROR REGISTER:", error);
+    return res.status(500).json({ error: "Error en el servidor" });
+    }
 };
 
 module.exports = { registrar };
